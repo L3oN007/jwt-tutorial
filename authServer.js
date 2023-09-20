be852482615f53e4d555fc8e258698d9c6c41012
Minh Nhật <89580650+L3oN007@users.noreply.github.com>
2023-09-20 23:45:42 +0700

    update data

--- authServer.js
+++ authServer.js
@@ -7,47 +7,125 @@ dotenv.config();
 const app = express();
 const PORT = 5500;
 
+const users = [
+  {
+    id: 1,
+    username: 'user',
+    password: '123',
+    role: 'user',
+  },
+  {
+    id: 2,
+    username: 'staff',
+    password: '123',
+    role: 'staff',
+  },
+  {
+    id: 3,
+    username: 'admin',
+    password: '123',
+    role: 'admin',
+  },
+]; // Store user data in-memory (for demonstration purposes)
+
 app.use(express.json());
 
-let refreshTokens = [];
+app.post('/auth/register', async (req, res) => {
+  try {
+    const { username, password, role } = req.body;
 
-app.post('/refreshToken', (req, res) => {
-  const refreshToken = req.body.token;
-  if (!refreshToken) res.sendStatus(401);
-  if (!refreshTokens.includes(refreshToken)) res.sendStatus(403);
+    // Check if the username already exists
+    const existingUser = users.find((user) => user.username === username);
+    if (existingUser) {
+      return res.status(400).json({ message: 'Username already exists' });
+    }
 
-  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, data) => {
-    console.log(err, data);
-    if (err) res.sendStatus(403);
-    const accessToken = jwt.sign(
-      { username: data.username },
-      process.env.ACCESS_TOKEN_SECRET,
-      {
-        expiresIn: '30s',
-      }
-    );
-    res.json({ accessToken });
-  });
+    // Create a new user object
+    const newUser = { username, password, role };
+    users.push(newUser);
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 });
 
-app.post('/login', (req, res) => {
-  // Authentication
-  // Authorization
-  // { username: 'Test' }
-  const data = req.body;
-  console.log({ data });
-  const accessToken = jwt.sign(data, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: '30s',
-  });
-  const refreshToken = jwt.sign(data, process.env.REFRESH_TOKEN_SECRET);
-  refreshTokens.push(refreshToken);
-  res.json({ accessToken, refreshToken });
+app.post('/auth/login', (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Find the user by username
+    const user = users.find((user) => user.username === username);
+    if (!user) {
+      return res.status(401).json({ message: 'Authentication failed' });
+    }
+
+    // Check the password
+    if (password !== user.password) {
+      return res.status(401).json({ message: 'Authentication failed' });
+    }
+
+    // Generate a random ID (for demonstration purposes)
+
+
+    // Generate an access token
+    const accessToken = jwt.sign({ id: user.id, username, role: user.role }, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: '10s', // Set the expiration time as desired
+    });
+
+    // Generate a refresh token
+    const refreshToken = jwt.sign({ id: user.id, username }, process.env.REFRESH_TOKEN_SECRET);
+
+    // Response data
+    const responseData = {
+      id: user.id,
+      username,
+      role: user.role,
+      accessToken,
+      refreshToken,
+    };
+
+    res.json(responseData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+
+app.post('/auth/refreshToken', (req, res) => {
+  try {
+    const refreshToken = req.body.refreshToken;
+
+    // Verify the refreshToken
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, data) => {
+      if (err) {
+        return res.status(403).json({ message: 'Invalid refresh token' });
+      }
+
+      // Find the user by username
+      const user = users.find((user) => user.username === data.username);
+      if (!user) {
+        return res.status(403).json({ message: 'Invalid refresh token' });
+      }
+
+      // Generate a new access token
+      const accessToken = jwt.sign({ username: data.username, role: user.role }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1m', // Set the expiration time as desired
+      });
+
+      res.json({ accessToken });
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 });
 
 app.post('/logout', (req, res) => {
-  const refreshToken = req.body.token;
-  refreshTokens = refreshTokens.filter((refToken) => refToken !== refreshToken);
-  res.sendStatus(200);
+  // For demonstration purposes, we don't need to do anything here
+  res.status(200).json({ message: 'Logged out successfully' });
 });
 
 app.listen(PORT, () => {
---
+++ users.js
@@ -0,0 +1,19 @@
+const users = [
+    {
+        username: 'user1',
+        passwordHash: '$2b$10$1sG46hCjVfIEe.Vz.3SRs.dF5zZ9pQsAqMjTxJ5aQILK2jQ5wsL0q', // Password is "123"
+        role: 'user',
+    },
+    {
+        username: 'user2',
+        passwordHash: '$2b$10$1sG46hCjVfIEe.Vz.3SRs.dF5zZ9pQsAqMjTxJ5aQILK2jQ5wsL0q', // Password is "123"
+        role: 'user',
+    },
+    {
+        username: 'admin',
+        passwordHash: '$2b$10$1sG46hCjVfIEe.Vz.3SRs.dF5zZ9pQsAqMjTxJ5aQILK2jQ5wsL0q', // Password is "123"
+        role: 'admin',
+    },
+];
+
+export default users;
